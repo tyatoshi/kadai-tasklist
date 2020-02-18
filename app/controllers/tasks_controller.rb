1ff505de8a38164f3bbc7ed019920b22d25f30b0
tyatoshi <>
2020-02-18 23:36:10 +0000

    add pagination

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
   ]
   
   def index
-    @tasks =Task.all.order(created_at: :desc)
+    @tasks =Task.all.order(created_at: :desc).page(params[:page]).per(10)
   end
   
   def show
