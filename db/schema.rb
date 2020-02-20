1dcc29d5cd822cb30f5ba3dc69c2cd6067624aab
tyatoshi <>
2020-02-20 07:41:56 +0000

    gem modify

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2020_02_18_070102) do
 
-  create_table "tasks", force: :cascade do |t|
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
