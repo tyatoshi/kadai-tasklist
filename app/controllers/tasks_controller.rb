f8458e9bed5a31117b097c1a8a17909f8615e2f9
tyatoshi <>
2020-03-11 08:51:21 +0000

    modify 20200311-17:51

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,4 @@
 class TasksController < ApplicationController
-  before_action :set_task, only:[:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
   before_action :ensure_correct_user, only:[:show, :edit, :update, :destroy]
   
@@ -49,10 +48,6 @@ class TasksController < ApplicationController
   end
   
   private
-  
-  def set_task
-    @task = Task.find_by(id: params[:id])
-  end
   
   def task_params
     params.require(:task).permit(:content,:status)
